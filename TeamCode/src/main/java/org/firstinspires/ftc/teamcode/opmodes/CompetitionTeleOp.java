027762d3175864abc7231ce23efcf82c1ee4a94b
kianbeeck <thebluesnake123@gmail.com>
2025-12-08 20:02:57 -0600

    Update CompetitionTeleOp.java
    delete lines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CompetitionTeleOp.java
@@ -1,11 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-
-
-
-
-
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
